1ba70c3ed7e779f577d1ec3ebdb49e422ae5eb8a
sigit yuki <sigityoeki@gmail.com>
2024-05-20 00:15:01 +0700

    edit main

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         ..add(const GetArticles()),
       child: MaterialApp(
         theme: theme(),
-        home: const DailyNews(),
+        // home: const DailyNews(),
       ),
     );
   }
